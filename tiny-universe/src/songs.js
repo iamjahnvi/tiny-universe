5d949f7427af49cdb8fd2d2c287f7dc0b0af8663
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-08-15 21:06:53 +0530

    songs file

---
+++ tiny-universe/src/songs.js
@@ -0,0 +1,19 @@
+const songs = [
+  {
+    title: "Cosmic Study",
+    artist: "The Mountain",
+    url: "https://www.youtube.com/watch?v=iYYRH4apXDo&list=RDiYYRH4apXDo&start_radio=1"
+  },
+  {
+    title: "Universe",
+    artist: "AlexGrohi",
+    url: "https://www.youtube.com/watch?v=DtVBCG6ThDk&list=RDDtVBCG6ThDk&start_radio=1"
+  },
+  {
+    title: "Silent Universe",
+    artist: "Universfield",
+    url: "https://www.youtube.com/watch?v=2EIeUlvHAiM&list=RD2EIeUlvHAiM&start_radio=1"
+  },
+];
+
+export default songs;
